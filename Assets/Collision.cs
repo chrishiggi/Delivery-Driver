cce82596175b7da79ab2913f6329cb7057485fb8
chrishiggi <chrishiggi@gmail.com>
2022-10-24 14:41:20 -0700

    added logic to change color of car when it has package

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    bool hasPackage = false;
+   bool hasPackage = false;
 
-    [SerializeField] float destroyDelay = 0.5f;
-    private void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("Collision...");
-    }
+   [SerializeField] float destroyDelay = 0.25f;
+   [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+   [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Package" && !hasPackage) {
-            Debug.Log("Package Picked Up...");
-            hasPackage = true;
-            Destroy(other.gameObject, destroyDelay);
-        }
+   SpriteRenderer spriteRenderer;
 
-        if (other.tag == "Customer" && hasPackage) {
-            Debug.Log("Package Delivered...");
-            hasPackage = false;
-        }
-    }
+   private void Start()
+   {
+      spriteRenderer = GetComponent<SpriteRenderer>();
+   }
+
+   private void OnCollisionEnter2D(Collision2D other)
+   {
+      Debug.Log("Collision...");
+   }
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.tag == "Package" && !hasPackage)
+      {
+         Debug.Log("Package Picked Up...");
+         hasPackage = true;
+         spriteRenderer.color = hasPackageColor;
+
+         Destroy(other.gameObject, destroyDelay);
+      }
+
+      if (other.tag == "Customer" && hasPackage)
+      {
+         Debug.Log("Package Delivered...");
+         hasPackage = false;
+         spriteRenderer.color = noPackageColor;
+      }
+   }
 }
