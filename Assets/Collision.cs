5414284fe77e8ca13288e8b6da57490a8f9612cb
chrishiggi <chrishiggi@gmail.com>
2022-10-24 14:29:33 -0700

    added multiple packages and logic to prevent multiple packages from being picked up

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+
+    [SerializeField] float destroyDelay = 0.5f;
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Collision...");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Package") {
+        if (other.tag == "Package" && !hasPackage) {
             Debug.Log("Package Picked Up...");
-            
+            hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
 
-        if (other.tag == "Customer") {
+        if (other.tag == "Customer" && hasPackage) {
             Debug.Log("Package Delivered...");
+            hasPackage = false;
         }
     }
 }
