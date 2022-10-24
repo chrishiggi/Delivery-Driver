fb8d008718a165bc412c6a2af7929bef426ed0b8
chrishiggi <chrishiggi@gmail.com>
2022-10-24 12:35:24 -0700

    added Trigger to Square asset

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Collision...");
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Trigger hit...");
+    }
 }
