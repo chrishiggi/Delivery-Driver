3050c1117f0c4046871ad82ed224d08729bf8687
chrishiggi <chrishiggi@gmail.com>
2022-10-24 14:17:30 -0700

    creaetd a package and delivery game object and updated triggers

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,20 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Delivery : MonoBehaviour
+public class Collision : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Collision...");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Package") {
-            Debug.Log("Package Picked Up...");
-            
-        }
-
-        if (other.tag == "Customer") {
-            Debug.Log("Package Delivered...");
-        }
+        Debug.Log("Trigger hit...");
     }
 }
